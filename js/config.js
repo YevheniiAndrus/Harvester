f5e9155f35e708f4d814fb7a21e8f1247d8e2521
Yevhenii Andrus <yevhenii.andrus@intellias.com>
2024-11-20 17:21:11 +0200

    Test google

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window._config = {
     api: {
-        invokeUrl: 'https://pvv9wvjphl.execute-api.eu-central-1.amazonaws.com/dev/users' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://google.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
